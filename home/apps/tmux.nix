6a82ee36dc29d40a9e2bb0ccd2c466a060e741c9
Logan Donley <hello@logandonley.com>
2024-01-05 15:05:26 -0500

    feat: tmux yank support

--- home/apps/tmux.nix
+++ home/apps/tmux.nix
@@ -29,6 +29,12 @@
 
     # Fix the slow escape-time for vim
     set -sg escape-time 10
+
+    # Copy
+    set-window-option -g mode-keys vi
+    bind-key -T copy-mode-vi v send -X begin-selection
+    bind-key -T copy-mode-vi V send -X select-line
+    bind-key -T copy-mode-vi y send -X copy-pipe-and-cancel 'xclip -in -selection clipboard'
      '';
   };
 }
