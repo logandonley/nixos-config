9ab455260962cca19f3f0c0318388a35ab0122a1
Logan Donley <hello@logandonley.com>
2024-01-05 15:37:45 -0500

    fix: removing doom

--- home/apps/ide.nix
+++ home/apps/ide.nix
@@ -1,4 +1,4 @@
-{ pkgs, home, ... }:
+{ pkgs, ... }:
 {
   home.packages = with pkgs; [
     jetbrains.webstorm
@@ -13,16 +13,6 @@
   programs.emacs = {
     enable = true;
     package = pkgs.emacs29;
-  };
-
-  home.activationScripts.setupDoomEmacs = {
-    text = ''
-      # Clone doom
-      if [ ! -d ${home.homeDirectory}/.emacs.d ]; then
-        git clone --depth 1 https://github.com/doomemacs/doomemacs ~/.config/emacs
-        ${home.homeDirectory}/.config/emacs/bin/doom install
-      fi
-    '';
   };
 
   home.profileExtra = ''
