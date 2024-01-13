d963a8697800fa1188021acb2f61c12841aed397
Logan Donley <hello@logandonley.com>
2024-01-13 15:26:43 -0500

    feat: adding steam

--- hosts/temple/default.nix
+++ hosts/temple/default.nix
@@ -1,12 +1,11 @@
-{ pkgs, ... }:
-{
-  imports =
-    [ 
-      ../../system/system.nix
-      ./streamdeck.nix
-      ./resolve.nix
-      ./hardware-configuration.nix
-    ];
+{ pkgs, ... }: {
+  imports = [
+    ../../system/system.nix
+    ./streamdeck.nix
+    ./resolve.nix
+    ./hardware-configuration.nix
+    ./steam.nix
+  ];
 
   # Bootloader.
   boot.loader.systemd-boot.enable = true;
@@ -18,27 +17,19 @@
   # Enable networking
   networking.networkmanager.enable = true;
 
-
   # Enable the X11 windowing system.
   services.xserver = {
     enable = true;
     videoDrivers = [ "nvidia" ];
 
-    displayManager = {
-      gdm.enable = true;
-    };
+    displayManager = { gdm.enable = true; };
 
-    desktopManager = {
-      gnome.enable = true;
-    };
+    desktopManager = { gnome.enable = true; };
 
     excludePackages = [ pkgs.xterm ];
   };
 
-  hardware.opengl = {
-    enable = true;
-  };
-
+  hardware.opengl = { enable = true; };
 
   # Configure keymap in X11
   services.xserver = {
@@ -46,7 +37,6 @@
     xkbVariant = "";
   };
 
-
-  system.stateVersion = "23.11"; 
+  system.stateVersion = "23.11";
 
 }
