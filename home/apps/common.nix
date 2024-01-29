87ba0d2d4487c44820e583aacc573eb059b729c7
Logan Donley <hello@logandonley.com>
2024-01-29 09:03:29 -0500

    feat: adding cloudflared

--- home/apps/common.nix
+++ home/apps/common.nix
@@ -63,6 +63,8 @@
     maestral-gui
 
     anytype
+    
+    cloudflared
   ];
 
 }
--- home/apps/ide.nix
+++ home/apps/ide.nix
@@ -1,5 +1,4 @@
-{ pkgs, ... }:
-{
+{ pkgs, ... }: {
   home.packages = with pkgs; [
     jetbrains.webstorm
     jetbrains.pycharm-professional
@@ -9,6 +8,52 @@
 
   programs.vscode = {
     enable = true;
+    # extensions = with pkgs.vscode-extensions; [
+    #   "mads-hartmann.bash-ide-vscode"
+    #   "github.copilot"
+    #   "github.copilot-chat"
+    #   "github.github-vscode-theme"
+    #   "golang.go"
+    #   "bbenoist.nix"
+    #   "jnoortheen.nix-ide"
+    #   "bradlc.vscode-tailwindcss"
+    #   "vscodevim.vim"
+    # ];
+    # settings = {
+    #   "workbench.colorTheme" = "GitHub Light";
+    #   "window.zoomLevel" = 2;
+    #   "vim.handleKeys" = {
+    #     "<C-d>" = true;
+    #     "<C-p>" = false;
+    #   };
+    #   "vim.leader" = "<space>";
+    #   "vim.useSystemClipboard" = true;
+    #   "extensions.experimental.affinity" = { "vscodevim.vim" = 1; };
+    #   "editor.formatOnSave" = true; #   "[templ]" = { "editor.defaultFormatter" = "a-h.templ"; };
+    #   "tailwindCSS.includeLanguages" = { "templ" = "html"; };
+    #   "emmet.includeLanguages" = { "templ" = "html"; };
+    #   "editor.linkedEditing" = true;
+    #   "html.autoClosingTags" = true;
+    # };
+    # keybindings = [
+    #   {
+    #     key = "ctrl+l";
+    #     command = "-workbench.action.chat.clear";
+    #     when = "hasChatProvider && inChat";
+    #   }
+    #   {
+    #     key = "ctrl+l";
+    #     command = "editor.action.inlineSuggest.commit";
+    #     when =
+    #       "inlineSuggestionHasIndentationLessThanTabSize && inlineSuggestionVisible && !editorHoverFocused && !editorTabMovesFocus && !suggestWidgetVisible";
+    #   }
+    #   {
+    #     key = "tab";
+    #     command = "-editor.action.inlineSuggest.commit";
+    #     when =
+    #       "inlineSuggestionHasIndentationLessThanTabSize && inlineSuggestionVisible && !editorHoverFocused && !editorTabMovesFocus && !suggestWidgetVisible";
+    #   }
+    # ];
   };
 
   programs.emacs = {
