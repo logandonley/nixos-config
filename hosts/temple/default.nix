1970a9ba6ff27fe0e3e1f8228122363e7728a5d4
Logan Donley <hello@logandonley.com>
2024-03-08 10:27:27 -0500

    switching to lightdm

--- hosts/temple/default.nix
+++ hosts/temple/default.nix
@@ -24,14 +24,14 @@
     videoDrivers = [ "nvidia" ];
 
     displayManager = {
-      gdm.enable = true;
+      # gdm.enable = true;
+      lightdm = { enable = true; };
       defaultSession = "none+qtile";
       sessionCommands = ''
         ${pkgs.xorg.xrandr}/bin/xrandr --output HDMI-0 --off --output DP-0 --mode 2560x1440 --rate 74.92 --pos 5120x0 --rotate left --output DP-1 --off --output DP-2 --mode 2560x1440 --rate 164.96 --pos 0x299 --rotate normal --output DP-3 --off --output DP-4 --primary --mode 2560x1440 --rate 144 --pos 2560x299 --rotate normal --output DP-5 --off
       '';
     };
 
-    # windowManager.awesome = { enable = true; };
     windowManager.qtile = {
       enable = true;
       extraPackages = python3Packages: with python3Packages; [ qtile-extras ];
