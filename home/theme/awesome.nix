161f79b8be26fbf3019dfb8d4c2d0477788b9292
Logan Donley <hello@logandonley.com>
2024-06-29 12:30:22 -0400

    changing default font

--- home/theme/awesome.nix
+++ home/theme/awesome.nix
@@ -8,7 +8,7 @@
       package = pkgs.gnome.gnome-themes-extra;
     };
     font = {
-      name = "Iosevka Nerd Font";
+      name = "IBM Plex Sans";
       size = 10;
     };
     iconTheme = { name = "Adwaita-dark"; };
